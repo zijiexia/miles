29ac1ed56110e12296cc5a54df2e57f59d6beb0c
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-10-24 11:03:17 +0800

    Split offload to train and rollout (#77)

--- miles/utils/arguments.py
+++ miles/utils/arguments.py
@@ -76,11 +76,20 @@ def get_miles_extra_args_provider(add_custom_arguments=None):
                 ),
             )
             parser.add_argument(
-                "--offload",
+                "--offload-train",
                 action="store_true",
                 default=False,
                 help=(
-                    "Whether to offload the rollout generator and training actor to CPU during training. "
+                    "Whether to offload the training actor to CPU during training. "
+                    "This will always be true when --colocate is set."
+                ),
+            )
+            parser.add_argument(
+                "--offload-rollout",
+                action="store_true",
+                default=False,
+                help=(
+                    "Whether to offload the rollout generator to CPU during training. "
                     "This will always be true when --colocate is set."
                 ),
             )
@@ -1203,7 +1212,7 @@ def miles_validate_args(args):
             args.actor_num_gpus_per_node = min(8, args.rollout_num_gpus)
             args.actor_num_nodes = args.rollout_num_gpus // args.actor_num_gpus_per_node
         args.colocate = False
-        args.offload = False
+        args.offload_train = args.offload_rollout = False
 
     assert not (args.debug_rollout_only and args.debug_train_only), (
         "debug_rollout_only and debug_train_only cannot be set at the same time, " "please set only one of them."
@@ -1211,7 +1220,7 @@ def miles_validate_args(args):
 
     # always true on offload for colocate at the moment.
     if args.colocate:
-        args.offload = True
+        args.offload_train = args.offload_rollout = True
         if args.rollout_num_gpus != args.actor_num_gpus_per_node * args.actor_num_nodes:
             print(
                 f"rollout_num_gpus {args.rollout_num_gpus} != actor_num_gpus_per_node {args.actor_num_gpus_per_node} "
